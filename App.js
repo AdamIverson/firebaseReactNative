667a05cbd17ba6de418ced8f7101481e934df87e
Adam Iverson <aciverso@gmail.com>
2022-11-06 13:48:05 -0600

    init?

---
+++ App.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { Button, Text, View, StyleSheet } from 'react-native';
+
+// React Native Styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
+const HelloWorld = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <View
+      style={styles.container}>
+      <Text>Hello, world!</Text>
+      <Text>You clicked {count} times!</Text>
+      <Button
+        onPress={() => setCount(count + 1)}
+        title="Click me!"
+      />
+    </View>
+  )
+}
+export default HelloWorld;
